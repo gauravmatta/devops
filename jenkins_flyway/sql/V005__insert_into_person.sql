4d8a1ceb2d069764b3ef0c8b634fe9dd3d10091d
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-05-25 01:41:42 +0530

    added values

---
+++ jenkins_flyway/sql/V005__insert_into_person.sql
@@ -0,0 +1,3 @@
+Insert into Persons (PersonID,LastName,FirstName,Address,City) values(1,'Lampard','Sudo','BG 65','New Delhi');
+Insert into Persons (PersonID,LastName,FirstName,Address,City) values(1,'Pampa','Steve','BG 92','Mumbai');
+Insert into Persons (PersonID,LastName,FirstName,Address,City) values(1,'Muttu','Muddu','BG 76','Kolkata');
